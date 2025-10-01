da362c9acf0780c0d420daea78d7ac30edef2f2b
Bryce Pavey <bryce@dreamwalk.com.au>
2025-10-01 17:35:35 +1000

    update implementation to use transparent text color on hidden input rather than opacity due to being unable to focus the input on new architecture

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React, { useCallback, useEffect, useImperativeHandle, useRef, useState } from 'react';
 import {
-    I18nManager,
-    StyleSheet,
-    Text,
-    TextInput,
-    View,
+  I18nManager,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
 } from 'react-native';
 import Animated, {
-    Easing,
-    cancelAnimation,
-    runOnJS,
-    useAnimatedStyle,
-    useSharedValue,
-    withRepeat,
-    withSequence,
-    withTiming,
+  Easing,
+  cancelAnimation,
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withTiming,
 } from 'react-native-reanimated';
 
 const styles = StyleSheet.create({
@@ -301,7 +301,7 @@ const SmoothPinCodeInput = React.forwardRef((props, forwardedRef) => {
         }}
         style={{
           flex: 1,
-          opacity: 0,
+          color: 'transparent',
           textAlign: 'center',
         }}
         testID={testID || undefined}
@@ -312,5 +312,3 @@ const SmoothPinCodeInput = React.forwardRef((props, forwardedRef) => {
 });
 
 export default SmoothPinCodeInput;
-
-
